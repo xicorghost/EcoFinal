ce390a11442a2e619f7e8e865e15e074f6637984
xicor <jord.diazz@duocuc.cl>
2025-07-06 15:11:58 -0400

    correcion para que se pueda leer

--- src/main/java/com/example/EcoFinal/EcoFinalApplication.java
+++ src/main/java/com/example/EcoFinal/EcoFinalApplication.java
@@ -3,7 +3,7 @@ package com.example.EcoFinal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
 public class EcoFinalApplication {
 
 	public static void main(String[] args) {
