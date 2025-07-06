a31787261c68c7e98ff8fb88e2b157da619a5ff4
xicor <jord.diazz@duocuc.cl>
2025-07-05 20:35:59 -0400

    prov model

--- src/main/java/com/example/Producto/Repository/ProductoRepository.java
+++ src/main/java/com/example/Producto/Repository/ProductoRepository.java
@@ -1,5 +1,8 @@
 package com.example.Producto.Repository;
 
-public class ProductoRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Producto.Model.Producto;
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {}
--- src/main/java/com/example/Producto/Service/ProductoService.java
+++ src/main/java/com/example/Producto/Service/ProductoService.java
@@ -1,5 +1,47 @@
 package com.example.Producto.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Producto.Model.Producto;
+import com.example.Producto.Repository.ProductoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductoService {
-    
+    @Autowired
+    private ProductoRepository productoRepository;
+
+
+    public List<Producto> listarProductos() {
+        return productoRepository.findAll();
+    }
+
+
+    public Producto guardarProducto(Producto p) {
+        return productoRepository.save(p);
+    }
+
+
+    public Optional<Producto> obtenerProductoPorId(Long id) {
+        return productoRepository.findById(id);
+    }
+
+
+    public Producto actualizarProducto(Long id, Producto producto) {
+        Producto existente = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el producto"));
+        existente.setNombre(producto.getNombre());
+        existente.setStock(producto.getStock());
+        existente.setPrecio(producto.getPrecio());
+        existente.setCategoria(producto.getCategoria());
+        return productoRepository.save(existente);
+    }
+
+
+    public void eliminarProducto(Long id) {
+        productoRepository.deleteById(id);
+    }
 }
